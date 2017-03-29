5f8ea97fe79dc24c2dddaa457c096218d2d1fb21
Tim Matthews <tim@3sidedcube.com>
2017-03-29 14:48:18 +0100

    Added a notification when a geofence event occurs

---
+++ android/lib/src/main/java/com/cube/geofencing/NotificationCancelReceiver.java
@@ -0,0 +1,27 @@
+package com.cube.geofencing;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by tim on 29/03/2017.
+ */
+public class NotificationCancelReceiver extends BroadcastReceiver
+{
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		//Cancel your ongoing Notification
+		Log.d("RNRM", "NotificationCancelReceiver " + intent.getExtras());
+
+		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		int notificationId = intent.getIntExtra("notificationId", 0);
+		if (notificationId != 0)
+		{
+			notificationManager.cancel("checkInRequest", notificationId);
+		}
+	}
+}
--- android/lib/src/main/java/com/cube/geofencing/RNRegionTransitionService.java
+++ android/lib/src/main/java/com/cube/geofencing/RNRegionTransitionService.java
@@ -1,7 +1,12 @@
 package com.cube.geofencing;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.facebook.react.HeadlessJsTaskService;
@@ -37,6 +42,19 @@ public class RNRegionTransitionService extends HeadlessJsTaskService
 			return null;
 		}
 
+		if (!intent.getBooleanExtra("launchHeadless", false))
+		{
+			showNotification(intent);
+			return null;
+		}
+
+		NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+		int notificationId = intent.getIntExtra("notificationId", 0);
+		if (notificationId != 0)
+		{
+			notificationManager.cancel("checkInRequest", notificationId);
+		}
+
 		WritableMap location = Arguments.createMap();
 		location.putDouble("latitude", geofencingEvent.getTriggeringLocation().getLatitude());
 		location.putDouble("longitude", geofencingEvent.getTriggeringLocation().getLongitude());
@@ -45,7 +63,7 @@ public class RNRegionTransitionService extends HeadlessJsTaskService
 		region.putString("identifier", geofencingEvent.getTriggeringGeofences().get(0).getRequestId());
 
 		WritableArray regionIdentifiers = Arguments.createArray();
-		for (Geofence triggered: geofencingEvent.getTriggeringGeofences())
+		for (Geofence triggered : geofencingEvent.getTriggeringGeofences())
 		{
 			regionIdentifiers.pushString(triggered.getRequestId());
 		}
@@ -60,5 +78,58 @@ public class RNRegionTransitionService extends HeadlessJsTaskService
 
 		Log.d(TAG, "Report geofencing event to JS: " + jsArgs);
 		return new HeadlessJsTaskConfig(RNRegionMonitorModule.TRANSITION_TASK_NAME, jsArgs, 0, true);
+	}
+
+	private void showNotification(Intent intent)
+	{
+
+		String packageName = getApplicationContext().getPackageName();
+		int smallIconResId = getApplicationContext().getResources().getIdentifier("ic_notification", "mipmap", packageName);
+		if (smallIconResId == 0)
+		{
+			smallIconResId = getApplicationContext().getResources().getIdentifier("ic_launcher", "mipmap", packageName);
+
+			if (smallIconResId == 0)
+			{
+				smallIconResId = android.R.drawable.ic_dialog_info;
+			}
+		}
+
+		int notificationID = (int)System.currentTimeMillis();
+
+		Intent notificationIntent = new Intent(getApplicationContext(), RNRegionTransitionService.class);
+		notificationIntent.putExtras(intent);
+		notificationIntent.putExtra("launchHeadless", true);
+		PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(), notificationID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		Intent yesActionIntent = new Intent(getApplicationContext(), RNRegionTransitionService.class);
+		yesActionIntent.putExtras(intent);
+		yesActionIntent.putExtra("launchHeadless", true);
+		yesActionIntent.putExtra("notificationId", notificationID);
+		PendingIntent yesPendingActionIntent = PendingIntent.getService(getApplicationContext(),
+		                                                                notificationID,
+		                                                                yesActionIntent,
+		                                                                PendingIntent.FLAG_UPDATE_CURRENT);
+
+		Intent noActionIntent = new Intent(getApplicationContext(), NotificationCancelReceiver.class);
+		noActionIntent.putExtra("notificationId", notificationID);
+		PendingIntent noPendingActionIntent = PendingIntent.getBroadcast(getApplicationContext(),
+		                                                                 notificationID,
+		                                                                 noActionIntent,
+		                                                                 PendingIntent.FLAG_UPDATE_CURRENT);
+
+		Notification notification = new NotificationCompat.Builder(getApplicationContext()).setContentTitle("O2 Touch session nearby!")
+		                                                                                   .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
+		                                                                                   .setPriority(NotificationCompat.PRIORITY_HIGH)
+		                                                                                   .setAutoCancel(false)
+		                                                                                   .setContentText("Would you like to check in to this session?")
+		                                                                                   .setSmallIcon(smallIconResId)
+		                                                                                   .addAction(0, "Yes", yesPendingActionIntent)
+		                                                                                   .addAction(0, "No", noPendingActionIntent)
+		                                                                                   .setVibrate(new long[]{0, 300L})
+		                                                                                   .build();
+
+		NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify("checkInRequest", notificationID, notification);
 	}
 }
